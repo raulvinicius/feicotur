9f3509c1865bdde4038488af6c9d7dd54e1abeeb
Raissa Costa <raissa.metal@gmail.com>
2017-05-16 20:45:18 -0300

    carrossel atual (parcial)

--- wp-content/themes/feicotur/index.php
+++ wp-content/themes/feicotur/index.php
@@ -3,12 +3,48 @@
 
 
 <section class="home">
-	<section class="carousel">
-		<a href="#" class="lk b">Mais sobre nós</a>
-		<h3 class="color-b">Inovação</h3>
-		<p>Em treze anos de história, muitas foram<br> as mudanças e inovações sem abrir mão de nossa qualidade na prestação de nossos serviços, que tem sido o nosso diferencial na conquista de novos parceiros.</p>
-		<h2 class="color-b">Institucional</h2>
+
+	<section class="carrossel container-fluid">
+		<div class="row">
+			
+			<ul class="edicoes container">
+				<div class="row">
+					
+					<li class="atual col-sm-offset-2 col-sm-8">
+						<h2 class="regular">Você é nosso convidado para</h2>
+
+						<img class="logo-atual" src="<?php bloginfo("template_url") ?>/img/feicotur-2017.png">
+
+						<p class="black">
+							<span class="color-y">14</span>, 
+							<span class="color-y">15</span> e 
+							<span class="color-y">16</span> de 
+							<span class="color-y">Julho</span>
+
+							<span class="linha-2 regular">Estacionamento do Estádio </span>
+							<span class="linha-3 regular color-y">Augustinho Lima</span>
+
+							<img src="<?php bloginfo("template_url") ?>/img/feicotur-seta-baixo-carousel.png">
+						</p>
+
+						<ul class="atracoes">
+							<li><span class="color-y black">S</span>hows</li>
+							<li><span class="color-y black">C</span>ircuito Regional de Quadrilhas</li>
+							<li><span class="color-y black">C</span>omidas Típicas</li>
+							<li><span class="color-y black">F</span>eira do Comércio</li>
+							<li><span class="color-y black">M</span>ostra de Artesanato</li>
+						</ul>
+
+						<a href="#" class="lk">Saber mais</a>
+
+					</li>
+
+				</div>
+			</ul>
+
+		</div>
 	</section>
+
 </section>
 
 
