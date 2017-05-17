17aa2e7b794403d455945b6d2396d16ce266e75a
Raissa Costa <raissa.metal@gmail.com>
2017-05-17 04:13:01 -0300

    carrossel 80%

--- wp-content/themes/feicotur/index.php
+++ wp-content/themes/feicotur/index.php
@@ -4,45 +4,51 @@
 
 <section class="home">
 
-	<section class="carrossel container-fluid">
-		<div class="row">
-			
-			<ul class="edicoes container">
-				<div class="row">
-					
-					<li class="atual col-sm-offset-2 col-sm-8">
-						<h2 class="regular">Você é nosso convidado para</h2>
+	<section class="carrossel">
 
-						<img class="logo-atual" src="<?php bloginfo("template_url") ?>/img/feicotur-2017.png">
+		<ul class="edicoes">
+				
+			<li class="atual container-fluid">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-offset-2 col-sm-8">
+							
+							<h2 class="regular">Você é nosso convidado para</h2>
 
-						<p class="black">
-							<span class="color-y">14</span>, 
-							<span class="color-y">15</span> e 
-							<span class="color-y">16</span> de 
-							<span class="color-y">Julho</span>
+							<img class="logo-atual" src="<?php bloginfo("template_url") ?>/img/feicotur-2017.png">
 
-							<span class="linha-2 regular">Estacionamento do Estádio </span>
-							<span class="linha-3 regular color-y">Augustinho Lima</span>
+							<p class="black">
+								<span class="color-y">14</span>, 
+								<span class="color-y">15</span> e 
+								<span class="color-y">16</span> de 
+								<span class="color-y">Julho</span>
 
-							<img src="<?php bloginfo("template_url") ?>/img/feicotur-seta-baixo-carousel.png">
-						</p>
+								<span class="linha-2 regular">Estacionamento do Estádio </span>
+								<span class="linha-3 regular color-y">Augustinho Lima</span>
 
-						<ul class="atracoes">
-							<li><span class="color-y black">S</span>hows</li>
-							<li><span class="color-y black">C</span>ircuito Regional de Quadrilhas</li>
-							<li><span class="color-y black">C</span>omidas Típicas</li>
-							<li><span class="color-y black">F</span>eira do Comércio</li>
-							<li><span class="color-y black">M</span>ostra de Artesanato</li>
-						</ul>
+								<img src="<?php bloginfo("template_url") ?>/img/feicotur-seta-baixo-carousel.png">
+							</p>
 
-						<a href="#" class="lk">Saber mais</a>
+							<ul class="atracoes">
+								<li><div class="divisor"></div><p>Shows</p><div class="divisor"></div></li>
+								<li><div class="divisor"></div><p>Circuito Regional de Quadrilhas</p><div class="divisor"></div></li>
+								<li><div class="divisor"></div><p>Comidas Típicas</p><div class="divisor"></div></li>
+								<li><div class="divisor"></div><p>Feira do Comércio</p><div class="divisor"></div></li>
+								<li><div class="divisor"></div><p>Mostra de Artesanato</p><div class="divisor"></div></li>
+							</ul>
 
-					</li>
+							<a href="#" class="lk">Saber mais</a>
 
+							<button class="bt-left">Anterior</button>
+							<button class="bt-right">Próximo</button>
+							
+						</div>
+					</div>
 				</div>
-			</ul>
+			</li>
 
-		</div>
+		</ul>
+
 	</section>
 
 </section>
