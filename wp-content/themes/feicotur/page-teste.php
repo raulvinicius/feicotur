24ff156b2f746d6fdddc1030bd9d2dce1fa15d3e
Raul Vinicius <raul.portifolio@gmail.com>
2017-05-19 05:14:34 -0300

    criando area de testes, testando primeiros elementos

---
+++ wp-content/themes/feicotur/page-teste.php
@@ -0,0 +1,54 @@
+<?php get_header(); ?>
+
+
+
+<section class="teste">
+
+	<section class="feicotur-head container-fluid">
+
+		<h3>Texto</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis placeat sunt similique laborum officia, vel fugiat fugit, aliquam necessitatibus pariatur culpa temporibus deleniti vero repellendus quia animi laboriosam, repudiandae ipsam.</p>
+
+		<!-- Generator: Adobe Illustrator 19.2.1, SVG Export Plug-In  -->
+		<svg class="recorte" version="1.1"
+			 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+			 x="0px" y="0px" width="100%" height="190" viewBox="0 0 100% 100%" style="enable-background:new 0 0 1921 100%;"
+			 xml:space="preserve">
+			 <defs>
+			   <pattern id='image' width="1" height="1" viewBox="0 0 100% 100%" preserveAspectRatio="none">
+			     <image xlink:href='<?php bloginfo('template_url') ?>/img/feicotur-bg-head-bot.jpg' x="0" y="0" width="100%" height="531" preserveAspectRatio="xMidYMin slice"></image>
+			   </pattern>
+			 </defs>
+			<g>
+				<path class="myClass" d="M189,0L0,189V0H190z"/>
+				</g>
+		</svg>
+
+		<span id="enfeite-centro"></span>
+
+
+
+<!-- <svg id='triangle' xmlns="http://www.w3.org/2000/svg" version="1.1" width='300px' height='300px'>
+    <path d='M0 0 L300 0 L300 300 Z' class='myClass'></path>
+</svg>
+ -->
+
+	</section>
+		<span class="enfeite-corpo-01"></span>
+		<span class="enfeite-corpo-02"></span>
+
+
+	<section class="quem-somos">
+
+		<div class="enfeite-titulo">
+			<h1>Quem Somos</h1>
+		</div>
+		
+	</section>
+
+
+</section>
+
+
+
+<?php get_footer(); ?>
