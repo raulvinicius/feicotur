225b7ef5e804d9fba5cea2fa92689c7e9a36b491
Raul Vinicius <raul.portifolio@gmail.com>
2017-05-23 09:25:41 -0300

    feito o responsivo da pagina A FEIRA

--- wp-content/themes/feicotur/page-a-feira.php
+++ wp-content/themes/feicotur/page-a-feira.php
@@ -42,20 +42,22 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12 tit-section">
-						<div id="wrap-tit" class="col-sm-4">
+						<div id="wrap-tit" class="col-lg-4 col-md-5">
 							<h2 class="black color-y">Parcerias</h2>
 						</div>
-						<div class="col-sm-8">
+						<div class="col-lg-8 col-md-7">
 							<p>Para garantir o máximo de desempenho da sua participação na FEICOTUR estabelecemos uma estreita parceria com vários órgãos Governamentais e não Governamentais como:</p>
 						</div>
 					</div>
 					<div class="col-sm-12">
 						<ul>
-							<li id="governo" title="Governo de Brasília"><i></i><p>Governo de Brasília</p></li>
-							<li id="adm" title="Administração Regional de Sobradinho"><i></i><p>Administração Regional de Sobradinho</p></li>
-							<li id="facidf" title="FACIDF"><i></i><p>FACIDF</p></li>
-							<li id="cacb" title="CACB"><i></i><p>CACB</p></li>
-							<li id="acis" title="ACIS"><i></i><p>ACIS</p></li>
+							<div class="col-xs-12">
+								<li id="governo" title="Governo de Brasília"><i></i><p>Governo de Brasília</p></li>
+								<li id="adm" title="Administração Regional de Sobradinho"><i></i><p>Administração Regional de Sobradinho</p></li>
+								<li id="facidf" title="FACIDF"><i></i><p>FACIDF</p></li>
+								<li id="cacb" title="CACB"><i></i><p>CACB</p></li>
+								<li id="acis" title="ACIS"><i></i><p>ACIS</p></li>
+							</div>
 						</ul>
 					</div>
 				</div>
@@ -72,10 +74,10 @@
 				<div class="row">
 					<div class="col-sm-12 tit-section">
 						
-						<div id="wrap-tit" class="col-sm-4">
+						<div id="wrap-tit" class="col-lg-4 col-md-5">
 							<h2 class="black color-y">Objetivos</h2>
 						</div>
-						<div class="col-sm-8">
+						<div class="col-lg-8 col-md-7">
 							<p>Cultuar as tradições locais, incentivando a participação ativa da população, não só da cidade como do Distrito Federal, em atividades sócio-culturais, religiosas, esportivas, cívicas, de negócios, educacionais e demais áreas que promovam a integração entre as pessoas, assim como:</p>
 						</div>
 
@@ -83,41 +85,44 @@
 
 					<div class="col-sm-12">
 						<ul>
-							<li class="col-md-4">
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Estimular Expositores</h3>
 								<p>Estimular a participação de expositores dos diversos ramos da indústria, comércio e do turismo a fim de exporem seus produtos e serviços ao público presente</p>
 							</li>
-							<li class="col-md-4">
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Revitalizar a Indústria</h3>
 								<p>Revitalizar a indústria, comércio e turismo da cidade, sendo esta sua maior expectativa</p>
 							</li>
-							<li class="col-md-4">
+							<div class="clearfix hidden-xs hidden-md hidden-lg"></div>
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Incentivar o Turismo</h3>
 								<p>Incentivar o turismo urbano e rural como fator de desenvolvimento sócio-econômico e de afirmação dos valores culturais e histórico-locais</p>
 							</li>
 
-							<div class="clearfix"></div>
+							<div class="clearfix hidden-xs hidden-sm"></div>
 
-							<li class="col-md-4">
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Integrar Empresários</h3>
 								<p>Incentivar a integração e participação de empresários na Feira da Industria, Comércio e Turismo, estimulando o planejamento de ações para melhor inserção nos diversos setores da economia do Distrito Federal</p>
 							</li>
-							<li class="col-md-4">
+							<div class="clearfix hidden-xs hidden-md hidden-lg"></div>
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Dinamizar a Indústria</h3>
 								<p>Dinamizar a indústria, comércio e turismo em Sobradinho</p>
 							</li>
-							<li class="col-md-4">
-								<h3>Promover a Aprocimação</h3>
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+								<h3>Promover a Aproximação</h3>
 								<p>Promover a aproximação entre produtores de bens e serviços e consumidores</p>
 							</li>
 
-							<div class="clearfix"></div>
+							<div class="clearfix hidden-xs hidden-sm"></div>
+							<div class="clearfix hidden-xs hidden-md hidden-lg"></div>
 
-							<li class="col-md-4">
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Incentivar e Difundir a Arte</h3>
 								<p>Incentivar e difundir as formas de produção artístico-literária locais por meio de apresentações de grupos musicais</p>
 							</li>
-							<li class="col-md-4">
+							<li class="col-md-4 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 								<h3>Fortalecer a Economia</h3>
 								<p>Fortalecer a economia local através da efetivação de negócios</p>
 							</li>
